3cd0c72829cef417a3fc55bb8f84366971cf52ae
cysaw <miloreinmann@gmail.com>
2026-08-06 13:52:52 +0200

    Added Pokedex Site

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const BASE_URL = "https://pokeapi.co/api/v2/";
 
-function fetch() {
+async function fetchPokemon(name) {
+    const response = await fetch(`${BASE_URL}pokemon/${name}`);
+    const pokemon = await response.json();
 
+    document.getElementById("pokemon-container").innerHTML = `
+        <h2>${pokemon.name}</h2>
+        <h1>${pokemon.order}</h2>
+        <img class="pokemon-normal-sprite" src ="${pokemon.sprites.other["official-artwork"].front_default}">
+    `;
 }
+
+fetchPokemon("rayquaza");
